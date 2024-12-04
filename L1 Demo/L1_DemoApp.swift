77c241bdaa19f462ad7a0b13b467950b5e9ef253
Kiyegga Francis <kiyeggafrancis1@gmail.com>
2024-12-04 20:27:08 +0300

    Initial Commit

---
+++ L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by kiyegga francis on 2024-12-04.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
